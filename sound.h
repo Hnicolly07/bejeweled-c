1c6a18a075ef4d6b5e115a4b0a1927e19ad9107b
IagodeFra <iagofgrcampos@gmail.com>
2026-07-22 17:10:06 -0300

    Versão final para áudio

--- sound.h
+++
@@ -1,6 +0,0 @@
-#include "raylib.h"
-/*Definição da música a ser tocada e o efeito sonoro a ser tocado quado as gemas quebram*/
-Music musica_jogo;
-Sound som_gemas_quebram;
-/* Usar essa função quando ouver eliminação de gemas*/
-void Tocar_som_gemas_quebrando(bool gemas_quebram);
