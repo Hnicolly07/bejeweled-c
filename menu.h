772304c211a64022b9fd0b50cf33fc0b3974a717
IagodeFra <iagofgrcampos@gmail.com>
2026-07-22 09:00:28 -0300

    Add files via upload

---
+++ menu.h
@@ -0,0 +1,17 @@
+#ifndef menu.h
+#define menu.h
+
+#include "raylib.h"
+
+void Logica_menu(void) {
+    /*Lógica da tela de menu*/
+
+}
+void Desenho_menu(void) {
+    /*Desenho da tela de Menu*/
+    BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+        
+
+}
